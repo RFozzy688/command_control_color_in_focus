fee0a889efc2a1c61868dd97eabe4f260b757f62
RFozzy688 <pokerfozzy@gmail.com>
2023-09-28 16:46:15 +0300

    start

---
+++ command_control_color_in_focus/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace command_control_color_in_focus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
